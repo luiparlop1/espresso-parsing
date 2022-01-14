e6f37533b32a27c75123ce73136377fd89185a04
Luis <luiparlop1@alum.us.es>
2022-01-14 17:22:56 +0100

    Quitando proyectos irrelevantes

--- Actualizado/app/src/androidTest/java/esadrcanfer/us/alumno/autotesting/objectivefunctions/dynamic/DynamicObjectiveFunction.java
+++
@@ -1,7 +0,0 @@
-package esadrcanfer.us.alumno.autotesting.objectivefunctions.dynamic;
-
-import esadrcanfer.us.alumno.autotesting.inagraph.actions.Action;
-
-public interface DynamicObjectiveFunction {
-    public double evaluate(Action action, String appPackage);
-}
